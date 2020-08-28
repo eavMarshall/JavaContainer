eada23454e3f7edcc2d7f9ea997441d5254df094
eavMarshall <elliottavmarshall@gmail.com>
2020-08-28 23:54:52 +1200

    test another case works

--- src/test/CreateSingleInstanceTest.java
+++ src/test/CreateSingleInstanceTest.java
@@ -2,8 +2,10 @@ package test;
 
 import DIJ.DIJContainer;
 import org.junit.Test;
+import test.testClasses.NoConstructor;
 import test.testClasses.SingleInstanceClass;
 
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 
 public class CreateSingleInstanceTest {
@@ -13,6 +15,15 @@ public class CreateSingleInstanceTest {
         SingleInstanceClass call1 = container.getInstanceOf(SingleInstanceClass.class);
         SingleInstanceClass call2 = container.getInstanceOf(SingleInstanceClass.class);
 
-        assertSame(call1, call2);
+        assertSame("Classes that implement SingleInstance should be kept by the contianer", call1, call2);
+    }
+
+    @Test
+    public void testContianerReturnsNewInstances() {
+        DIJContainer container = new DIJContainer();
+        NoConstructor call1 = container.getInstanceOf(NoConstructor.class);
+        NoConstructor call2 = container.getInstanceOf(NoConstructor.class);
+
+        assertNotSame("Classes that don't implement SingleInstances should be new instances", call1, call2);
     }
 }
