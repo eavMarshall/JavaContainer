d24b970901885360538cfe640ff35881b27fab28
eavMarshall <elliottavmarshall@gmail.com>
2020-08-28 23:47:13 +1200

    testing missed case

--- src/test/AddOverrideRuleTest.java
+++ src/test/AddOverrideRuleTest.java
@@ -13,11 +13,18 @@ public class AddOverrideRuleTest {
     public void testOverride() {
         DIJContainer container = new DIJContainer();
         DIJContainer overrideContainer = container.addOverrideRule(OneConstructor.class, new NoConstructor());
+        DIJContainer overrideContainer2 = container.addOverrideRule(OneConstructor.class, new NoConstructor());
 
         assertNotSame("Container should be immutable, new container per override rule", container, overrideContainer);
+        assertNotSame("Container should be immutable, new container per override rule", container, overrideContainer2);
+        assertNotSame("Container should be immutable, new container per override rule", overrideContainer, overrideContainer2);
 
         Object instance = overrideContainer.getInstanceOf(OneConstructor.class);
-
         assertTrue(NoConstructor.class.isAssignableFrom(instance.getClass()));
+
+        Object instance2 = overrideContainer2.getInstanceOf(OneConstructor.class);
+        assertTrue(NoConstructor.class.isAssignableFrom(instance2.getClass()));
+
+        assertNotSame(instance, instance2);
     }
 }
